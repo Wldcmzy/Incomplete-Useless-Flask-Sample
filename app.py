c85dad2b8b4a6142f2dd651a5a74eeb8eb9530fd
Wldcmzy <wldcmzy@163.com>
2022-09-21 08:55:11 +0800

    调整

--- app.py
+++ app.py
@@ -12,17 +12,24 @@ app.register_blueprint(modules.caricature.blue)
 app.register_blueprint(modules.SDW.blue)
 CORS(app, supports_credentials=True)
 
+def judge_url_pass(url : str) -> bool:
+    pass_url: list[str] = ['/login', '/regist']
+    pass_prefix: list[str] = ['/static/nologin']
+    if url in pass_url: return True
+    for each in pass_prefix: 
+        if url.startswith(each): return True 
+    return False
+
 @app.before_request
 def before():
     url = request.path
-    passUrl = ['/login', '/regist']
-    if url in passUrl:
+    if judge_url_pass(url):
         pass
     else:
         username = session.get('username', None)
         cookie = session.get('cookie', None)
         if not cookie or not modules.master.check_cookie(username, cookie):
-            return redirect('/login')
+            return redirect('login')
         else:
             pass
 
